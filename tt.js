4027a1b504e76d6d931fc4ff75b4a21713455f2b
utente <ghetto23@live.it>
2022-02-08 16:11:49 +0100

    cleaning avg by script removed fake last sell val

--- tt.js
+++ tt.js
@@ -28,32 +28,38 @@ export const getAvg = async (tokenProto) => {
      * @property {number} a.isBuy
      */
 
-    //filter results
+        //filter results
     let today_avg = avg?.filter(a => (a.filledTimeSeconds * 1000) > start)
     if (today_avg === undefined || today_avg.length <= 0) return 0
 
-    //get last trade (add method to get last few sell and get lowest)
-    let lastOrder = avg?.reduce((last, val) => val.filledTimeSeconds > last.filledTimeSeconds ? val : last);
+    //get last trade
+    avg = avg.slice().sort((a, b) => b.filledTimeSeconds - a.filledTimeSeconds)
+    let lastOrder = avg[0];
 
     //reduce object to primitive
     today_avg = today_avg.map(x => x.takerAssetFilledAmount)
 
     //remove outliers
     today_avg = Utils.filterOutliers(today_avg);
-    if (today_avg[0] * 10 < today_avg[today_avg.length-1])
-        today_avg = today_avg.filter(x=> x < today_avg[today_avg.length-1] * 0.05)
+    if (today_avg[0] * 10 < today_avg[today_avg.length - 1])
+        today_avg = today_avg.filter(x => x < today_avg[today_avg.length - 1] * 0.05)
 
     //get avg
     let sum_price = Math.abs(today_avg.reduce((a, b) => (a + b)) / 1000000000000000000);
     let avg_price = (sum_price / today_avg.length || 0);
     let last_price = (lastOrder.takerAssetFilledAmount / 1000000000000000000);
-    if (last_price > (avg_price * 10)){console.error(`BOT_TRAP_DETECTED: outlier_last_sell ${last_price} > ${avg_price * 10}`); return 0;}
+
+    let i = 0;
+    while (last_price > (avg_price * 10) && i <= avg.length) {
+        console.error(`BOT_TRAP_DETECTED: outlier_last_sell ${last_price} > ${avg_price * 10}`);
+        last_price = avg[++i]
+    }
 
     return {
         last_price: parseFloat(last_price.toString()).toFixed(6),
         avg_price: parseFloat(avg_price.toString()).toFixed(6),
         last_date: parseInt(((new Date().getTime() - new Date(lastOrder?.filledTimeSeconds * 1000)) / 60000).toString()),
-        daily_amount:today_avg.length
+        daily_amount: today_avg.length
     }
 }
 
