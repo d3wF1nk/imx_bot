31469e72a6d326f38361d8ba0a03006b5e095512
d3wF1nk <26278869+d3wF1nk@users.noreply.github.com>
2022-02-10 00:35:05 +0100

    Update index.js
    Fixed error

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function loop(client) {
                     if (avg !== 0)
                         if (!(vars.MAX_TIME) || avg.last_date < vars.MAX_TIME)
                             if (!(vars.MIN_AMOUNT_TRADE) || avg.daily_amount > vars.MIN_AMOUNT_TRADE)
-                                if (avg.avg_price > Utils.formatEther(p.sell.data.quantity))
+                                if (avg.avg_price > Utils.formatEther(p.buy.data.quantity))
                                     if (avg.last_price > Utils.formatEther(p.buy.data.quantity)) {
                                         const diff = Utils.comparePrice(Utils.formatEther(p.buy.data.quantity), avg.avg_price)
                                         console.log(`[id:${p.order_id}] [avg:${avg.avg_price}] [${avg.last_date}m => last_price:${avg.last_price}] [actual_price:${Utils.formatEther(p.buy.data.quantity)}] ${p.sell.data.properties.name} (${diff?.sign}${diff?.value?.toFixed(1)}%)${diff.alert ? '⚠ ⚠ ⚠' : ''}`)
